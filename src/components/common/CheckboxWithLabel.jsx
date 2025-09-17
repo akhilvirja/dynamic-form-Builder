3dc5a9ce24d6fe1bcabcad5cc049f879a2ab5ae6
Akhil-virja <virjaakhil555@gmail.com>
2025-09-17 15:34:03 +0530

    app crash on clicking checkbox bug resolve

--- src/components/common/CheckboxWithLabel.jsx
+++ src/components/common/CheckboxWithLabel.jsx
@@ -2,11 +2,12 @@ import { Checkbox } from '@/components/ui/checkbox'
 import { Label } from '@/components/ui/label'
 import React from 'react'
 
-function CheckboxWithLabel({label, name, checked=false, handleChange}) {
+function CheckboxWithLabel({label, name, checked=false, handleChange, required = false}) {
   return (
     <>
         <Checkbox
             checked={checked}
+            required={required}
             onCheckedChange={(checked) =>
             handleChange(name, checked)
             }
